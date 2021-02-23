43c9508637fdc7271eb4e6e8b83f15d31e8356ed
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-02-24 00:47:09 +0530

    Added 133_A

---
+++ 1300 - 1399/5 HQ9+.cpp
@@ -0,0 +1,17 @@
+//5 HQ9+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(true);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	string p;
+	cin>>p;
+	if(p.find('H')!=string::npos || p.find('Q')!=string::npos || p.find('9')!=string::npos)
+	cout<<"YES\n";
+	else
+	cout<<"NO\n";
+	return 0;
+}
