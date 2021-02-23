ca63c5c238ea38fbf05717b6d77a3da62df429c2
naresh2002 <naresh6436@gmail.com>
2021-02-23 11:56:54 +0530

    Added templates for ladder till 1599

---
+++ 1500 - 1599/0 (TEMPLATE).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define PB push_back
+#define int long long
+#define endl '\n'
+const int MOD = (int)1e9 + 7;
+const double PI = acos(-1.0);
+using namespace std;
+
+
+int32_t main () {
+  _
+  
+  return 0;
+}
