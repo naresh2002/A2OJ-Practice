e598e5e164c3b5c33aeecc322becb8cf6d055cd2
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-02-27 21:41:41 +0530

    Added 445_A

---
+++ 0800 - 1299/60 DZY Loves Chessboard.cpp
@@ -0,0 +1,42 @@
+//60 DZY Loves Chessboard
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(true);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+    long long n,m;
+	cin>>n>>m;
+    char a[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i][j]=='.')
+            {
+                if((i+j)%2==0)
+                a[i][j]='B';
+                else
+                a[i][j]='W';
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
