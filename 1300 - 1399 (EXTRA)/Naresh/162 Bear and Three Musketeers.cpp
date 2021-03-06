51971df11da6edac034929beca0c60888ac2b291
naresh2002 <naresh6436@gmail.com>
2021-03-06 14:57:16 +0530

    Added 574_B

--- 1300 - 1399 (EXTRA)/Naresh/162 Bear and Three Musketeers.cpp
+++ 1300 - 1399 (EXTRA)/Naresh/162 Bear and Three Musketeers.cpp
@@ -20,53 +20,25 @@ int32_t main () {
     check[a][b] = 1;
     check[b][a] = 1;
   }
-  //cout << "\n---\n";
-  //for (int i = 1; i <= n; i++) {
-    //for (int j = 1; j <= n; j++) {
-      //cout << i << " " << j << " " << check[i][j] << "\n";
-    //}
-    //cout << endl;
-  //}
-  //cout << "---\n";
   
-  auto comp = [&](vector<int> &p, vector<int>&q) {
-    return p.size() < q.size();
-  };
-  sort(v, v + n + 1, comp);
+  int ans = 1e9;
   
-  //for(int i = 0; i <= n; i++) {
-    //cout << v[i].size() << " -- ";
-    //for (auto x : v[i]) {
-      //cout << x << " ";
-    //}
-    //cout << endl;
-  //}
-  
-  int ans = -1;
-  //int i = 0;
-  //while (v[i].size() < 2) {
-    //i++;
-  //}
-  
-  //cout << "NEXT\n" << endl;
   for (int i = 0; i <= n; i++) { //  i is first friend
     for (auto x : v[i]) {       //  x is second friend
       for (auto y : v[x]) {     //  y is third firend
         if (check[i][y]) {
-          ans = v[i].size() + v[x].size() + v[y].size() - 6;
-          cout << i << " " << x << " " << y << "\n";
-          cout << v[i].size() << " " << v[x].size() << " " << v[y].size() << endl << endl;
+          int k = v[i].size() + v[x].size() + v[y].size() - 6;
+          ans = min(ans, k);
         }
-        if (ans >= 0) {
-          break;
-        }
-      }
-      if (ans >= 0) {
-        break;
       }
     }
   }
-  cout << max(-1LL, ans);
+  if (ans == 1e9) {
+    cout << -1;
+  }
+  else {
+    cout << ans;
+  }
   
   return 0;
 }
